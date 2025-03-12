e4955f340e4ed59d4dba2de533505011dc12df2e
byeongkookkang <byeongguk1998@gmail.com>
2025-03-12 12:02:36 +0900

    lc_45_jump-game-2 2회차 풀이  주석 추가

--- greedy/lc_45_jump-game-2.py
+++ greedy/lc_45_jump-game-2.py
@@ -1,10 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        '''
+        n-1번 인덱스에 도달하기 위한 최소 점프 횟수를 반환하는 함수
+        Time Complexity : O ( n )
+        '''
         n = len(nums)
-        currentMaxJump=0 #현재 점프로 도달할 수 있는 최대 인덱스
-        nextMaxJump=0 #다음 점프로 도달할 수 있는 최대 인덱스
-
         count=0
+        # 점프를 count 번 했을때, 도달할 수 있는 최대 인덱스
+        currentMaxJump=0
+        # 점프를 count + 1 번 했을 때, 도달할 수 있는 최대 인덱스 
+        nextMaxJump=0 
+
         for i in range(n-1):
             nextMaxJump=max(nextMaxJump,nums[i]+i)
             if i == currentMaxJump:
